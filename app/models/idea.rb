71ad1006ae2006cce0824279de2fa4feab4dfe60
Marta <marta.bakaj@gmail.com>
2017-09-30 16:01:08 +0200

    adding tumbnails

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,7 @@
 class Idea < ApplicationRecord
 	mount_uploader :picture, PictureUploader
+	validates_presence_of :name 
+	validates :description, presence: true,
+							length: { minimum: 5, maximum: 60 }
+
 end
--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "should saved when have all params " do
+     image = Idea.new(name: "fgrfyhft", description:"dlugi tekst" )
+     assert image.save
+  end
+
+  test "should not be saved when have no name " do
+     image = Idea.new(description:" dlugi tekst" )
+     assert_not image.save
+  end
+  test "should not be saved when we have too short description " do
+     image = Idea.new(name:"random name", description:"a,.!" )
+     assert_not image.save
+  end
+  test "should not be saved when we have too long description " do
+     image = Idea.new(name:"random name", description:"a*60" )
+     assert_not image.save
+  end
+
 end
